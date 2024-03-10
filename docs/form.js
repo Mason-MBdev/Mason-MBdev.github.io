cb0829c2746e7d47d69c754b2e8bc99f2c36e303
Mason Barney <mason.barney@mbdev.ca>
2024-03-10 12:18:38 -0400

    Contact page - status flag updates
    Added better styled user feedback when submitting a form, also added "more >" to project containers for future drop-down functionality

--- docs/form.js
+++ docs/form.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     const contactForm = document.getElementById('contactForm');
     const statusMessage = document.getElementById('statusMessage');
-    
+    const statusDot = document.getElementById('statusDot');
+
     if (!contactForm || !statusMessage) {
         console.error('Error: Contact form or status message element not found.');
         return;
@@ -12,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
     
         // Create a FormData object from the form
         const formData = new FormData(contactForm);
-    
+
         // Send a POST request to the backend
         fetch('https://pi.mbdev.ca/submit_form', {
             method: 'POST',
@@ -28,12 +29,15 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(data => {
             // Update status message 
             statusMessage.innerText = data.message;
-            statusMessage.style.color = 'green'; // green for success
+            statusMessage.style.color = 'rgb(70, 255, 130)'; // green for success
+            statusDot.style.color = 'rgb(70, 255, 130)';
+
         })
         .catch(error => {
             // Update status message 
-            statusMessage.innerText = 'Connection closed: I am away';
-            statusMessage.style.color = 'red'; // red for error
+            statusMessage.innerText = 'Connection closed: server is temporarily offline';
+            statusMessage.style.color = 'rgb(255, 50, 50)'; // red for error
+            statusDot.style.color = 'rgb(255, 50, 50)';
             console.error('Error:', error);
         });
     });
