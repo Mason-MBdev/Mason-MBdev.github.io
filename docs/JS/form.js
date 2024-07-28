875fe20461772af24c00db7403490ba5a494cea0
Mason Barney <mason.barney@mbdev.ca>
2024-07-28 11:53:32 -0400

    Update form.js

--- docs/JS/form.js
+++ docs/JS/form.js
@@ -3,6 +3,11 @@ document.addEventListener('DOMContentLoaded', function() {
     const statusMessage = document.getElementById('statusMessage');
     const serverStatusElement = document.getElementById('serverState');
 
+    // test form mock data
+    const testFormData = new FormData();
+    testFormData.append('name', 'load');
+    testFormData.append('email', 'a@a');
+    testFormData.append('message', ' ');
 
     if (!contactForm || !statusMessage || !serverStatusElement) {
         console.error('Error: Contact form or status message element not found.');
@@ -12,7 +17,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Check server status when the page is loaded by sending a mock form POST, if there is a response then the server is online
     fetch('https://pi.mbdev.ca/submit_form', {
         method: 'POST',
-        body: new FormData()
+        body: testFormData
     })
     .then(response => {
         if (!response.ok) {
@@ -41,7 +46,7 @@ document.addEventListener('DOMContentLoaded', function() {
         })
         .then(response => {
             if (!response.ok) {
-                throw new Error('Error: Network response was not ok');
+                throw new Error('Network Error');
             }
             // Update status message 
             return response.json(); // Parse JSON response
@@ -54,7 +59,7 @@ document.addEventListener('DOMContentLoaded', function() {
         })
         .catch(error => {
             // Update status message 
-            statusMessage.innerText = 'Connection closed: server is temporarily offline';
+            statusMessage.innerText = 'Not sent';
             statusMessage.style.color = 'rgb(255, 50, 50)'; // red for error
             console.error('Error:', error);
         });
