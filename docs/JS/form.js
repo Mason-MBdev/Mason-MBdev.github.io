130c5d83db10860344d39b8553068feacd8a4486
Mason Barney <mason.barney@mbdev.ca>
2024-07-28 11:45:42 -0400

    Server Status check

--- docs/JS/form.js
+++ docs/JS/form.js
@@ -1,13 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
     const contactForm = document.getElementById('contactForm');
     const statusMessage = document.getElementById('statusMessage');
-    const statusDot = document.getElementById('statusDot');
+    const serverStatusElement = document.getElementById('serverState');
 
 
-    if (!contactForm || !statusMessage) {
+    if (!contactForm || !statusMessage || !serverStatusElement) {
         console.error('Error: Contact form or status message element not found.');
         return;
     }
+
+    // Check server status when the page is loaded by sending a mock form POST, if there is a response then the server is online
+    fetch('https://pi.mbdev.ca/submit_form', {
+        method: 'POST',
+        body: new FormData()
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Error: Network response was not ok');
+        }
+        serverStatusElement.innerText = 'Online';
+        serverStatusElement.style.color = 'rgb(70, 255, 130)'; // green for success
+    })
+    .catch(error => {
+        serverStatusElement.innerText = 'Offline';
+        serverStatusElement.style.color = 'rgb(255, 50, 50)'; // red for error
+        console.error('Error:', error);
+    });
+    
     
     contactForm.addEventListener('submit', function(event) {
         event.preventDefault();
@@ -31,14 +50,12 @@ document.addEventListener('DOMContentLoaded', function() {
             // Update status message 
             statusMessage.innerText = data.message;
             statusMessage.style.color = 'rgb(70, 255, 130)'; // green for success
-            statusDot.style.color = 'rgb(70, 255, 130)';
 
         })
         .catch(error => {
             // Update status message 
             statusMessage.innerText = 'Connection closed: server is temporarily offline';
             statusMessage.style.color = 'rgb(255, 50, 50)'; // red for error
-            statusDot.style.color = 'rgb(255, 50, 50)';
             console.error('Error:', error);
         });
     });
